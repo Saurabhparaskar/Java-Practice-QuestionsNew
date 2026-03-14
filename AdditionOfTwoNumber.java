de2d762d56a519c7ccc1c936fc51ab875fc8414a
Saurabh <Saurabh@DESKTOP-LFSK3NR>
2026-03-14 21:07:57 +0530

    Additon of two number

---
+++ AdditionOfTwoNumber.java
@@ -0,0 +1,13 @@
+package pratices;
+
+public class AdditionOfTwoNumber {
+	public static void main(String[]args) {
+		
+		int a=10;
+		int b=20;
+		System.out.println("Additon of two number "+ ( a +b));
+		
+	
+	}
+
+}
